0c9b48203d7eb45ddaae4498055697584d6c07b2
Лев <115810918+Leo-Volkov@users.noreply.github.com>
2025-03-14 15:35:35 +0300

    ButtonPushDisciplines

--- src/pages/main/Disciplines/index.tsx
+++ src/pages/main/Disciplines/index.tsx
@@ -1,16 +1,15 @@
+import { ButtonPushDisciplines } from '@/features/ButtonPushDisciplines'
 import { ButtonPush } from '@/shared/ui/ButtonPush'
 import { PageTitle } from '@/shared/ui/PageTitle'
 import { DisciplinesTable } from '@/widgets/DisciplinesTable'
 
 export function Disciplines() {
-  const handleClick = () => {
-    console.log('Предмет добавлен')
-  }
+
   return (
     <>
       <PageTitle>Дисциплины</PageTitle>
       <div className="mb-4">
-        <ButtonPush onClick={handleClick}>Добавить предмет</ButtonPush>
+        <ButtonPushDisciplines />
       </div>
       <DisciplinesTable />
     </>
